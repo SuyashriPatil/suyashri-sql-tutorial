177cc1d5d60ba3715a46516a68831f9c7f99322f
Suyashri Satappa Patil <suyashripatil@gmail.com>
2025-07-27 15:36:51 +0530

    Add files via upload
    "This repository contains a structured SQL tutorial that covers everything from basic commands like SELECT and INSERT to advanced topics like JOINs, subqueries, and window functions. It is designed for beginners, students, and aspiring data analysts or backend developers. All tutorials come with real-world examples and practice questions in .sql format, ready to run in MySQL, PostgreSQL, or SQLite."

---
+++ 1-CREATE DB AND TABLE.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employee(
+           name VARCHAR(100) NOT NULL,
+			  position VARCHAR(100) NOT NULL,
+			  age INT,
+			  department VARCHAR(100) ,
+			  date DATE,
+			  salary INT
+		 );
+
+insert into employee(name,position,age,department,date,salary)
+values('suyashri patil ','data analyst',22,'data science','2022-05-15',65000.00),
+	   ('alisha ','devops',23,'computer science','2021-09-20',75000.00),
+	   ('vaishnavi patil ','business communication',25,'computer science','2020-07-10',82000.00),
+	   ('nikita javadekar ','software engineer',24,'computer science','2018-12-5',58000.00),
+	   ('suhani lad ','web developer',22,'computer science','2019-10-25',62000.00);
+
+
+         
+SELECT * FROM employee;
