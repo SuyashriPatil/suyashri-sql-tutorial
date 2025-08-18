372f3a280892b17ba1d9e073d9afc1c4540bdd2e
Suyashri Satappa Patil <suyashripatil@gmail.com>
2025-08-18 17:40:32 +0530

    Add files via upload

---
+++ 21-COALESCE FUNCTION.sql
@@ -0,0 +1,66 @@
+CREATE TABLE products(
+    product_id SERIAL PRIMARY KEY,
+	product_name VARCHAR(100),
+	category VARCHAR(50),
+	price NUMERIC(10,2),
+	quantity INT,
+	added_date DATE,
+	discount_rate NUMERIC(5,2)
+);
+
+INSERT INTO products(product_name,category,price,quantity,added_date,discount_rate)VALUES
+('Laptop','Electronics',75000.50,10,'2024-01-15',10.00),
+('Smartphone','Electronics',45000.99,25,'2024-02-25',5.00),
+('Headphones','Accessories',2500.55,50,'2024-03-05',15.00),
+('Office Chair','Furniture',5500.50,20,'2024-12-01',20.00),
+('Desk','Furniture',7000.50,15,'2024-01-28',12.00),
+('Monitor','Electronics',12000.50,8,'2024-01-10',8.00),
+('Printer','Electronics',9500.50,5,'2024-02-1',7.58),
+('Mouse','Accessories',750.50,40,'2024-03-15',10.00),
+('Keyboard','Accessories',1250.50,35,'2024-03-18',10.00),
+('Tablet','Electronics',30000.50,12,'2024-02-28',5.00);
+
+
+SELECT * FROM products;
+
+ALTER TABLE products DROP COLUMN discount_price;
+
+
+ALTER TABLE products ADD COLUMN discount_price NUMERIC(10,2);
+
+
+UPDATE products SET discount_price = NULL 
+WHERE product_name IN('Laptop','Desk');
+
+
+
+UPDATE products SET discount_price = price * 0.9 
+WHERE product_name NOT IN('Laptop','Desk');
+
+
+SELECT product_name,price,discount_price FROM products;
+
+SELECT product_name,
+COALESCE(discount_price,price)AS final_price
+FROM products;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
